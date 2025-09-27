5ea116b1807455248a7b8f1d4c051906178fac3c
Takahiro Matsunari <takahiro.matsunari@curionlab.com>
2025-09-27 20:14:14 +0900

    chore: setup GitHub Pages deployment for playground-json
    - add deploy.yml for GitHub Actions
    - add package.json for examples/playground-json
    - update root package.json build script (tsup)
    - adjust Vite config and entrypoint for playground-json

--- examples/playground-json/vite.config.ts
+++ examples/playground-json/vite.config.ts
@@ -2,9 +2,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'examples/playground-json', // プロジェクトのルートを明示
+  root: '.', // プロジェクトのルートを明示
+  publicDir: 'public',
   build: {
-    outDir: '../../docs/json-playground', // GitHub Pages公開用の出力先
+    outDir: './dist', // GitHub Pages公開用の出力先
     emptyOutDir: true, // ビルド時に出力先をクリーンアップ
   },
 });
